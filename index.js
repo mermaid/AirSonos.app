9906cb6bc855d6fb93a003442cf536d764c2b83a
CameronH <cameronh@qualtrics.com>
2017-01-05 13:55:31 -0700

    Animated logo and images

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 (function() {
     'use strict';
 
+    console.log(process.version);
+
     const {app, BrowserWindow, Tray, Menu} = require('electron');
     const path = require('path');
+    const AirSonos = require('airsonos');
     // const ipc = require('ipc');
 
     var mainWindow = null;
+
+    let animationInterval;
+    let tray = null;
 
     app.on('ready', function() {
         // mainWindow = new BrowserWindow({
@@ -17,10 +23,9 @@
 
         // mainWindow.loadURL(`file://${__dirname }/app/index.html`);
 
-        var tray = null;
 
         if (process.platform === 'darwin') {
-            tray = new Tray(path.join(__dirname, 'img/tray-iconTemplate.png'));
+            tray = new Tray(path.join(__dirname, 'img/logo-iconTemplate.png'));
         }
         else {
             tray = new Tray(path.join(__dirname, 'img/tray-icon-alt.png'));
@@ -48,30 +53,48 @@
         var trayMenu = Menu.buildFromTemplate(trayMenuTemplate);
         tray.setToolTip('AirSonos');
         tray.setContextMenu(trayMenu);
-        var t = true;
+
+        console.log('Searching for Sonos devices on network...\n');
+
+        let instance = new AirSonos({
+            timeout: 5,
+        });
+
+        instance.start().then((tunnels) => {
+
+            tunnels.forEach((tunnel) => {
+            console.log(`${ tunnel.deviceName } (@ ${ tunnel.device.host }:${ tunnel.device.port }, ${ tunnel.device.groupId })`);
+            });
+
+            console.log(`\nSearch complete. Set up ${ tunnels.length } device tunnel${ tunnels.length === 1 ? '' : 's' }.`);
+        }).done();
+
         
-        const animate = function() {
-            var i = 0;
-            setInterval(function() {
-                tray.setImage(path.join(__dirname, 'img/animations/frame_' + i + '.png'));
-                i += 1;
-                i %= 8;
-            }, 200);
-
-        };
         animate();
-
+        setTimeout(stopAnimation, 5000);
     });
 
 
+    function animate() {
+        var i = 0;
+        var j = 1;
+        animationInterval = setInterval(function() {
+            if (i < 0 || i > 12) {
+                j *= -1;
+                i += j;
+            }
+            tray.setImage(path.join(__dirname, `img/animations/logo${i}-iconTemplate.png`));
+            
+            i += j;
+        }, 80);
+    }
 
-
-
-
-
-
-
-
+    function stopAnimation() {
+        if(animationInterval) {
+            clearInterval(animationInterval);
+            tray.setImage(path.join(__dirname, 'img/logo-iconTemplate.png'));
+        }            
+    }
 
 
 })();
